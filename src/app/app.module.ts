b9cd9323515ec71907564277bda40eac8d5417af
kabir_SK <kabir3rd@gmail.com>
2022-12-29 00:50:02 +0530

    added static bootstrap components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TemplateContentEditorComponent } from './template-content-editor/template-content-editor.component';
 import { TemplateComponent } from './template/template.component';
 import { TemplateItemComponent } from './template/template-item/template-item.component';
 import { TemplateListComponent } from './template/template-list/template-list.component';
+import { CreateTemplateComponent } from './template/create-template/create-template.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,12 @@ import { TemplateListComponent } from './template/template-list/template-list.co
     TemplateContentEditorComponent,
     TemplateComponent,
     TemplateItemComponent,
-    TemplateListComponent
+    TemplateListComponent,
+    CreateTemplateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/template-content-editor/template-content-editor.component.ts
+++ src/app/template-content-editor/template-content-editor.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template-content-editor',
   templateUrl: './template-content-editor.component.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateContentEditorComponent implements OnInit {
 
-  constructor() { }
+  defaultType = "default"
+  variableType =["1", "2", "3", "default"];
+  templateVariables = ["text", "number", "date"];
+
+  @ViewChild('templateEditor', {static:true}) templateEditor! : ElementRef;
+
+  constructor(private renderer:Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+
+  onSubmitTemplateEditorContent() {
+    console.log(this.templateEditor.nativeElement.innerHTML);
+  }
+
+  onSaveTemplateVariable(form : NgForm) {
+    console.log(form);
   }
 
 }
