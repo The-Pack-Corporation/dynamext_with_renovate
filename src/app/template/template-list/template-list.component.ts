5006253237109ce2976aaaedba249c094c934d39
kabirpack <kabir3rd@gmail.com>
2023-06-10 00:12:10 +0530

    added icon packages and implemented search in template lists

--- src/app/template/template-list/template-list.component.ts
+++ src/app/template/template-list/template-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
 import { TemplateVariable } from 'src/app/models/template-variable.model';
 import { Template } from 'src/app/models/template.model';
 import { TemplateService } from 'src/app/services/template.service';
@@ -28,13 +29,29 @@ export class TemplateListComponent implements OnInit {
 
   templateList: Template[] = [];
   templateToDelete: Template;
+  filteredTemplates: Template[] = [];
+  userTemplateSearchKey: string;
+  //debounce subject
+  userTemplateSearchKeyUpdate = new Subject<string>();
 
 
   @ViewChild('reactiveFormModal', {static : false}) reactiveFormModal : TextGeneratorFormComponent;
   @ViewChild('deleteConfirmation', {static:true}) deleteConfirmation: ConfirmationModalComponent;
 
 
-  constructor(private templateService: TemplateService) { }
+  constructor(private templateService: TemplateService) {
+
+    // Debounce search.
+    //it will fetch whatever changes the input box gives after debounce time. no matter how many time you
+    //give input before debounce time, the function wont execute
+    this.userTemplateSearchKeyUpdate.pipe(
+      debounceTime(400),
+      distinctUntilChanged())
+      .subscribe(value => {
+        this.filterResults(value);
+      });
+
+   }
 
   ngOnInit(): void {
 
@@ -47,6 +64,8 @@ export class TemplateListComponent implements OnInit {
     this.templateService.templateSelectedEvent.subscribe( (template: Template) => {
       this.reactiveFormModal.show(template);
     })
+
+
   }
 
   loadMyTemplates() {
@@ -64,6 +83,7 @@ export class TemplateListComponent implements OnInit {
           )
         )
       });      
+      this.filteredTemplates = this.templateList;
     })
 
   }
@@ -90,6 +110,19 @@ export class TemplateListComponent implements OnInit {
   onDeleteTemplateCancelled() {
     this.templateToDelete = null;
   }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredTemplates = this.templateList;
+    }
+  
+    this.filteredTemplates = this.templateList.filter(
+      template => template?.templateName.toLowerCase().includes(text.toLowerCase()) 
+      || template?.templateContent.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
+
   
 
 
